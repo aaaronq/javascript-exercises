1ea8103784b61becc6a0cfab4888415a61ebd0a4
aaaronq <aaronquigley@mail.com>
2022-12-23 22:41:04 +0000

    Complete exercises 01-07 per fundamentals-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let repeatedString = "";
+    for (let i = 0; i < num; i++) {
+        if (i == 0) {
+            repeatedString = string
+        }
+        else {
+        repeatedString += string;
+        }
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
